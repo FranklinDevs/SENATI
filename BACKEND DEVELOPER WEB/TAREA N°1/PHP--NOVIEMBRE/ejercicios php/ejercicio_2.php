b318f34646f5c606de49f733cad72bed43a95b2c
FranklinDevs <fronaldbq@gmail.com>
2024-11-13 09:19:37 -0500

    Add files via upload

---
+++ BACKEND DEVELOPER WEB/TAREA N°1/PHP--NOVIEMBRE/ejercicios php/ejercicio_2.php
@@ -0,0 +1,10 @@
+<?php
+// 2 .- Crea dos variables y sumalas y muestra el resultado.
+$num1 = 5;
+$num2 = 10;
+
+$suma = $num1 + $num2;
+
+echo " La suma es: " . // sirve para concatenar   
+$suma;
+?>
